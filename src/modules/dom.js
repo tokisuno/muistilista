f567b43577b5f7072e66b05fe837fc3f459d157b
tokisuno <lucaswmei@gmail.com>
2024-08-13 11:54:23 -0400

    you can show and hide lists now with a button

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,13 +1,23 @@
 import { saveData, accessData } from './storage.js';
 
 let z = accessData();
-console.log(z)
+z.lists.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
 
 const body = document.querySelector('body');
 
+const newProject = document.createElement('button');
+newProject.setAttribute('class', 'newProject');
+newProject.textContent = "New Project";
+
+const newItem = document.createElement('button');
+newItem.setAttribute('class', 'newItem');
+newItem.textContent = "New Item";
+
+body.appendChild(newProject);
+body.appendChild(newItem);
+
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
-
 
 z.lists.find(list => {
     let todoList = document.createElement('div');
@@ -18,6 +28,29 @@ z.lists.find(list => {
     todoListName.textContent = `${list.name}`;
     todoList.append(todoListName);
 
+    let toggleAppearance = document.createElement('button');
+    toggleAppearance.textContent = "Show";
+    toggleAppearance.setAttribute('id', 'show');
+    toggleAppearance.addEventListener('click', (e) => {
+        toggleAppearance.textContent = `${toggleAppearance.textContent === "Show" ? "Hide" : "Show"}`;
+        switch (e.target.id) {
+            case "hide":
+                toggleAppearance.setAttribute('id', 'show');
+                hideList();
+                break;
+            case "show":
+                toggleAppearance.setAttribute('id', 'hide');
+                drawList(list, todoList);
+                break;
+        }
+    });
+    todoList.appendChild(toggleAppearance);
+    container.appendChild(todoList);
+});
+
+body.append(container);
+
+function drawList(list, todoList) {
     list.items.forEach(item => {
         let todoListItem = document.createElement('div');
         todoListItem.setAttribute('class', 'todoListItem');
@@ -39,27 +72,33 @@ z.lists.find(list => {
         }
 
         let priority = document.createElement('div');
-        priority.textContent = `${item.priority}`;
+        priority.textContent = `${(item.priority === 0 ? "Normal" : "Urgent")}`;
         priority.setAttribute('class', 'itemPriority');
+        todoListItem.appendChild(priority); 
+
+
+        let status = document.createElement('div');
+        status.textContent = `${(item.status === false ? "Incomplete" : "Complete")}`;
+        todoListItem.appendChild(status);
 
         let completeButton = document.createElement('button');
-        completeButton.textContent = `${item.status}`;
+        completeButton.textContent = "Toggle complete status";
         completeButton.setAttribute('class', 'toggleStatus');
         completeButton.addEventListener('click', () => {
             item.toggleStatus();
-            completeButton.textContent = `${item.status}`;
+            status.textContent = `${(item.status === false ? "Incomplete" : "Complete")}`;
             console.log(item);
         });
-
-        todoListItem.appendChild(priority); 
         todoListItem.appendChild(completeButton); 
-
         todoList.appendChild(todoListItem);
     });
-    container.appendChild(todoList);
-});
 
-body.append(container);
+}
+
+function hideList() {
+    let items = document.querySelectorAll("div.todoListItem").forEach(e => e.remove());
+    console.log(items);
+}
 
 window.onbeforeunload = function() {
     saveData(z);
